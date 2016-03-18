aa7b6c6952b63c5934ad6d48aab925b74dafab02
Krishan Wyse <kwysek@gmail.com>
2016-03-18 23:21:06 +0000

    Add unicode glyphs

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
 bat_home="/sys/class/power_supply/BAT0"
-if ! [[ -d ${bat_home} ]]; then
-    exit 1;
-fi
+if ! [[ -d $bat_home ]]; then exit 1; fi
 
-case `cat ${bat_home}/capacity` in
-    [0-9])
-	icons=""
-	rc=33;;
-    [1-2][0-9])
-	icon="";;
-    [3-6][0-9])
-	icon="";;
-    [7-8][0-9]|9[0-4])
-	icon="";;
-    9[5-9]|100)
-	icon="";;
+case `cat $bat_home/capacity` in
+[0-9])
+  icon=""
+  rc=33;;
+[1-2][0-9])
+  icon="";;
+[3-6][0-9])
+  icon="";;
+[7-8][0-9]|9[0-4])
+  icon="";;
+9[5-9]|100)
+  icon="";;
 esac
 
-if [[ `cat ${bat_home/status` != "Discharging" ]]; then
-    echo ""
-else
-    echo ${icon}
-fi
-
-exit ${rc - 0}
+# echo $icon
+# echo $icon
+# test `cat $bat_home/status` != "Discharging" && echo \#00FF00
+if [[ `cat $bat_home/status` != "Discharging" ]]; then echo ""
+else echo $icon; fi
+exit ${rc-0}
--- i3blocks/volume.sh
+++ i3blocks/volume.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-if [[ -z `command -v amixer` ]]; then
-    exit 1
-fi
+if [[ -z `command -v amixer` ]]; then exit 1; fi
 
 if [[ `amixer get Master | grep -om1 '\[\w*\]' | tr -d '[]'` == "off" ]]; then
-    printf '%-5s' "."
+  printf '%-5s' ""
 else
-    if [[ `amixer get Master | grep -om1 '\[[0-9]*%\]' | tr -d '[]%'` -gt 30 ]]; then
-	printf '%-5s' "."
-    else
-	print '%-5s' "."
-    fi
+  if [[ `amixer get Master | grep -om1 '\[[0-9]*%\]' | tr -d '[]%'` -gt 30 ]]; then
+    printf '%-5s' ""
+  else
+    printf '%-5s' ""
+  fi
 fi
 
 exit 0
