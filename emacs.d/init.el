81b24d0fe73419a24b7b59fb89a88d5322d8b47b
Krishan Wyse <kwysek@gmail.com>
2019-03-12 19:23:45 +0000

    Enable emacs packages and material theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,11 @@
 (global-display-line-numbers-mode)
+
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")))
+
+(setq package-selected-packages
+      '(material-theme))
+
+(load-theme 'material t)
