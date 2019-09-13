baea5f80f04bb2bfb5ecfaa6ff1392820f94451c
Krishan Wyse <kwysek@gmail.com>
2019-09-13 16:30:35 +0100

    Allow Emacs to find rustfmt and run it on save

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,7 +8,9 @@
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
-(use-package rust-mode)
+(use-package rust-mode
+  :init
+  (setq rust-format-on-save t))
 
 (use-package cargo
   :hook (rust-mode . cargo-minor-mode))
@@ -18,14 +20,22 @@
 
 (use-package atom-one-dark-theme)
 
+(defun get-shell-path-contents ()
+  "Returns the contents of $PATH based on the user's shell"
+  (replace-regexp-in-string "\n+$" "" (shell-command-to-string "$SHELL -c 'echo $PATH'")))
+
 (global-display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
 (menu-bar-no-scroll-bar)
 (column-number-mode)
 (tool-bar-mode 0)
 
+(setenv "PATH" (get-shell-path-contents))
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
+
+(setq exec-path (append exec-path (split-string (getenv "PATH") ":")))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
